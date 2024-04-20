689185b3d6fa4fcfec653adb3e3dc6e9edeee59d
Arjun Gill <arjungill6ep@gmail.com>
2024-04-20 14:02:27 +0100

    feat: setup NextUI and Tailwindcss

---
+++ client/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
+/** @type {import('tailwindcss').Config} */
+const config: Config = {
+  content: [
+    './index.html',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [nextui()],
+};
+export default config;
