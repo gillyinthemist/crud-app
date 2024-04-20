822164466ed6d08f05c5a9cd9498407e959c6589
Arjun Gill <arjungill6ep@gmail.com>
2024-04-20 09:06:25 +0100

    init: boilerplate react-vite & express-ts

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { router } from './router';
+
+const app = express();
+
+// Middleware set-up:
+// app.use(cors({
+//   origin: "https://symphonious-starlight-f902b2.netlify.app",
+//   optionsSuccessStatus: 200
+// }));
+app.use(express.json());
+app.use('/', router);
+
+// Server listening:
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
